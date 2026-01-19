dbecd6579816de63cb9b9d1035cb4928c0633235
DESKTOP-D0VFTGP\Admin <ivancovae@gmail.com>
2026-01-19 08:38:12 +0300

    Задание 6. Настройка Rate Limiting

---
+++ Task6/nginx_config.js
@@ -0,0 +1,24 @@
+http {
+    server_tokens off;
+    
+    limit_req_zone $binary_remote_addr zone=api_limit:10m rate=10r/m;
+    
+    # Настройка upstream для балансировки нагрузки
+    upstream backend_servers {
+        server backend1.example.com;
+        server backend2.example.com;
+        server backend3.example.com;
+    }
+
+    server {
+        listen 80;
+
+        location / {
+            limit_req zone=api_limit;
+            
+            limit_req_status 429;
+            
+            proxy_pass http://backend_servers;
+        }
+    }
+}
